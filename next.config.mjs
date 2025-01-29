c9ca8de78a136fe576b7b45e0e1ded9201938fc5
JuwelRana34 <juwelrana3426@gmail.com>
2025-01-29 18:24:24 +0600

    Refactor Firebase configuration and update Next.js settings; remove unused Firebase functions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output:"export",
+    images: {
+      unoptimized: true,
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'developerjuwelrana.web.app',
+              port: '',
+             
+            },
+        ]
+      },
 };
 
 export default nextConfig;
